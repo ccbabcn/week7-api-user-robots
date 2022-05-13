2fe94fbbb927ac1b5b4258165977f53e970256fb
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-13 21:11:26 +0200

    defined Robot model structure

---
+++ database/models/Robot.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+const RobotSchema = new Schema({
+  name: { type: String },
+  velocity: { type: Number },
+  resistance: { type: Number },
+  created: { type: String },
+  image: { type: String },
+});
+
+const Robot = model("Robot", RobotSchema, "robots");
+
+module.exports = Dog;

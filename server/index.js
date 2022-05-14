05bb31de14274e7159b8b5ed3ff99e653a60c927
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-14 20:17:02 +0200

    implemented and tested generalError middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const { robotsRouter } = require("./routers/robotRouters");
+const robotsRouter = require("./routers/robotRouters");
 
 const app = express();
 
--- server/middlewares/error.test.js
+++ server/middlewares/error.test.js
@@ -1,10 +1,10 @@
-const { notFoundError } = require("./errors");
+const { notFoundError, generalError } = require("./errors");
 
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
 describe("Given a notFoundError function", () => {
-  const res = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-  };
   describe("When it's invoked with a response", () => {
     test("Then it should call the response status method with a 404", () => {
       const expectedResult = 404;
@@ -13,10 +13,35 @@ describe("Given a notFoundError function", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedResult);
     });
+
     test("Then it should call the response json method with a msg '404 Page Not Found'", () => {
-      const expectedMsg = { msg: "404 Page Not Found" };
+      const expectedMsg = { msg: "404 endpoint Not Found" };
 
       notFoundError(null, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedMsg);
+    });
+  });
+});
+
+describe("Given a genralError function", () => {
+  const error = {
+    message: "error text",
+  };
+
+  describe("When it's invoked with a response", () => {
+    test("Then it should call the response status method with 500", () => {
+      const expectedStatus = 500;
+
+      generalError(error, null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+
+    test("Then it should call the response msg method with 'Internal server error'", () => {
+      const expectedMsg = { msg: "Internal server error" };
+
+      generalError(error, null, res);
 
       expect(res.json).toHaveBeenCalledWith(expectedMsg);
     });

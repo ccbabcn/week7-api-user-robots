e5438a46e36005c79f949da38d766f74dd754da2
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-14 18:08:29 +0200

    implemented and tested notfoundError middleware json  msg

--- server/middlewares/error.test.js
+++ server/middlewares/error.test.js
@@ -1,17 +1,24 @@
 const { notFoundError } = require("./errors");
 
 describe("Given a notFoundError function", () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
   describe("When it's invoked with a response", () => {
     test("Then it should call the response status method with a 404", () => {
       const expectedResult = 404;
 
-      const res = {
-        status: jest.fn(),
-      };
-
       notFoundError(null, res);
 
       expect(res.status).toHaveBeenCalledWith(expectedResult);
+    });
+    test("Then it should call the response json method with a msg '404 Page Not Found'", () => {
+      const expectedMsg = { msg: "404 Page Not Found" };
+
+      notFoundError(null, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedMsg);
     });
   });
 });
--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -2,7 +2,7 @@ const debug = require("debug")("robots:middlewares:errors");
 const chalk = require("chalk");
 
 const notFoundError = (req, res) => {
-  res.status(404);
+  res.status(404).json({ msg: "404 Page Not Found" });
   debug(chalk.redBright("Received a request for an unexisting endpoint"));
 };
 
